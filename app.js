444ab692ef39178edf79f62d73548cd12295fa62
Miroslav Zdravkovic <miroslav@dudesquare.nl>
2018-12-25 09:43:48 +0100

    Validation alert

--- app.js
+++ app.js
@@ -48,6 +48,18 @@ class UI {
         }
     }
 
+    static showAlert(message, className) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+
+        // Vanish in 3 seconds
+        setTimeout(() => document.querySelector('.alert').remove(), 3000);
+    }
+
     static clearFields() {
         document.querySelector('#title').value = '';
         document.querySelector('#author').value = '';
@@ -70,14 +82,20 @@ document.querySelector('#book-form').addEventListener('submit', e => {
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
 
-    // Instantiate book class
-    const book = new Book(title, author, isbn);
+    // Validation
+    if (title === '' || author === '' || isbn === '') {
+        UI.showAlert('Please fill in all fields', 'info');
+    } else {
+        // Instantiate book class
+        const book = new Book(title, author, isbn);
 
-    // Add book to UI
-    UI.addBookToList(book);
+        // Add book to UI
+        UI.addBookToList(book);
 
-    // Clear fields
-    UI.clearFields();
+        // Clear fields
+        UI.clearFields();
+    }
+
 })
 
 // Event: remove a book
